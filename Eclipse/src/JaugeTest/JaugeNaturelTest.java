2931eab32ec9e273659281d744f901fef335d6b5
DarkVasDehors <sibade.arnaud@gmail.com>
2017-10-20 15:10:33 +0200

    Debut des tests

--- Eclipse/src/JaugeTest/JaugeNaturelTest.java
+++ Eclipse/src/JaugeTest/JaugeNaturelTest.java
@@ -1,10 +1,33 @@
 package JaugeTest;
+//import org.junit.Before;
+import junit.framework.* ;
 
-public class JaugeNaturelTest {
+public class JaugeNaturelTest extends TestCase {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public TestResult run() {
+	     System.out.print("Test de JaugeNaturel:");
 
+	     System.out.print(".");
+	     testDansIntervalle();
+
+	      System.out.print(".");
+	      testInferieurIntervalle();
+
+	     System.out.println("OK");
+	  }
+	
+	public void testDansIntervalle(){
+		
+		try {
+			testVert();
+		} catch (Exception e) {
+			System.out.println("testVert à return true .... erreur");
+		}
+	}
+	
+	public void testVert() throws Exception {
+		JaugeNaturel maJauge = new JaugeNaturel(100,200,300);
+		assertTrue("le test jauge vert echoue ", maJauge.estVert());
 	}
 
 }
