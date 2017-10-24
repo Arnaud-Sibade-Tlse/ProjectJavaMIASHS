aca6ed7279b418e6be3608a3846d06030d5440cb
GuilhemS31 <guilhem31.susa@hotmail.com>
2017-10-24 17:05:49 +0200

    all JaugeNaturelTest

--- Eclipse/src/JaugeNaturel/JaugeNaturelTest.java
+++ Eclipse/src/JaugeNaturel/JaugeNaturelTest.java
@@ -63,13 +63,56 @@ public class JaugeNaturelTest {
 	@Test
 	public void testDansIntervalle(){
 		assertTrue("est Vert",jauge.estVert());
-		assertTrue("est pas Rouge",jauge.estRouge());
-		assertTrue("est pas Bleu",jauge.estBleu());
+		assertTrue("est pas Rouge",!jauge.estRouge());
+		assertTrue("est pas Bleu",!jauge.estBleu());
 	}
 	
 	@Test
 	public void testDeplacement(){
+		JaugeNaturel maJauge = new JaugeNaturel(10,20,15);
 		
+		maJauge.incrementer();
+		assertTrue("est Vert",jauge.estVert());
+		assertTrue("est pas Rouge",!jauge.estRouge());
+		assertTrue("est pas Bleu",!jauge.estBleu());
+		
+		maJauge.decrementer();
+		assertTrue("est Vert",jauge.estVert());
+		assertTrue("est pas Rouge",!jauge.estRouge());
+		assertTrue("est pas Bleu",!jauge.estBleu());
 	}
 	
+	@Test
+	public void testInferieurIntervalle(){
+		JaugeNaturel maJauge = new JaugeNaturel(10,20,8);
+		assertTrue("val < Min < Max",maJauge.getValeur() < maJauge.getMin() && maJauge.getMin() < maJauge.getMax());
+		
+		JaugeNaturel maJauge2 = new JaugeNaturel(10,20,10);
+		assertTrue("val = Min < Max",maJauge2.getValeur() == maJauge2.getMin() && maJauge2.getMin() < maJauge2.getMax());
+	}
+	
+	@Test
+	public void testLimiteVigieMaxInferieurVigieMin(){
+		JaugeNaturel maJauge = new JaugeNaturel(20,10,10);
+		assertTrue("Max : 10",maJauge.getMax() == 10);
+		assertTrue("Min : 20",maJauge.getMin() == 20);
+	}
+	
+	@Test
+	public void testMaxEgaleMin(){
+		JaugeNaturel maJauge = new JaugeNaturel(10,10,10);
+		assertTrue("val = Min = Max",maJauge.getValeur() == maJauge.getMin() && maJauge.getMin() == maJauge.getMax());
+		
+		JaugeNaturel maJauge2 = new JaugeNaturel(10,10,11);
+		assertTrue("val > Min == Max",maJauge2.getValeur() > maJauge2.getMin() && maJauge2.getMin() == maJauge2.getMax());
+	}
+	
+	@Test
+	public void testSuperieurIntervalle(){
+		JaugeNaturel maJauge = new JaugeNaturel(10,20,22);
+		assertTrue("Min < Max < val",maJauge.getValeur() > maJauge.getMax() && maJauge.getMin() < maJauge.getMax());
+		
+		JaugeNaturel maJauge2 = new JaugeNaturel(10,20,20);
+		assertTrue("Min < Max = val",maJauge2.getValeur() == maJauge2.getMax() && maJauge2.getMin() < maJauge2.getMax());
+	}
 }
