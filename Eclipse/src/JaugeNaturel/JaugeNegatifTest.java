63c1bffaf3dba0c2264914e83b1e0431c7a13ba1
DarkVasDehors <sibade.arnaud@gmail.com>
2017-12-04 15:58:52 +0100

    negtif frere

---
+++ Eclipse/src/JaugeNaturel/JaugeNegatifTest.java
@@ -0,0 +1,118 @@
+package JaugeNaturel;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class JaugeNegatifTest {
+
+	JaugeNaturel jauge;
+	
+	@Before
+	public void setUp() throws Exception {
+		jauge = new JaugeNaturel(-20,-10,-12);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		jauge = null;
+	}
+
+	@Test
+	public void testJaugeNaturel() {
+		assertNotNull("Jauge bien cree",jauge);
+	}
+
+	@Test
+	public void testEstRouge() {
+		assertTrue("Jauge rouge",jauge.estRouge());
+	}
+
+	@Test
+	public void testEstVert() {
+		assertTrue("Jauge verte",jauge.estVert());
+	}
+
+	@Test
+	public void testEstBleu() {
+		assertTrue("Jauge bleu",jauge.estBleu());
+	}
+
+	@Test
+	public void testIncrementer() {
+		long currentVal = jauge.getValeur();
+		jauge.incrementer();
+		assertTrue("Jauge incrementee",jauge.getValeur() == currentVal + 1 );
+	}
+
+	@Test
+	public void testDecrementer() {
+		long currentVal = jauge.getValeur();
+		jauge.decrementer();
+		assertTrue("Jauge decrementee",jauge.getValeur() == currentVal - 1 );
+	}
+
+	@Test
+	public void testToString() {
+		String monToString = "<"+jauge.getValeur()+" ["+jauge.getMin()+"," + jauge.getMax() + "]>";
+		assertTrue("Jauge toString",monToString.equals(jauge.toString()));
+	}
+	
+	@Test
+	public void testDansIntervalle(){
+		assertTrue("est Vert",jauge.estVert());
+		assertFalse("N'est pas Rouge",jauge.estRouge());
+		assertFalse("N'est pas Bleu",jauge.estBleu());
+	}
+	
+	@Test
+	public void testDeplacement(){
+		JaugeNaturel maJauge = new JaugeNaturel(-20,-10,-15);
+		
+		maJauge.incrementer();
+		assertTrue("est Vert",jauge.estVert());
+		assertFalse("N'est pas Rouge",jauge.estRouge());
+		assertFalse("N'est pas Bleu",jauge.estBleu());
+		
+		maJauge.decrementer();
+		assertTrue("est Vert",jauge.estVert());
+		assertFalse("N'est pas Rouge",jauge.estRouge());
+		assertFalse("N'est pas Bleu",jauge.estBleu());
+	}
+	
+	@Test
+	public void testInferieurIntervalle(){
+		JaugeNaturel maJauge = new JaugeNaturel(-20,-10,-22);
+		assertTrue("val < Min < Max",maJauge.getValeur() < maJauge.getMin() && maJauge.getMin() < maJauge.getMax());
+		
+		maJauge = new JaugeNaturel(-20,-10,-20);
+		assertTrue("val = Min < Max",maJauge.getValeur() == maJauge.getMin() && maJauge.getMin() < maJauge.getMax());
+	}
+	
+	@Test
+	public void testLimiteVigieMaxInferieurVigieMin(){
+		JaugeNaturel maJauge = new JaugeNaturel(-10,-20,-10);
+		assertTrue("Max : 10",maJauge.getMax() == -10);
+		assertTrue("Min : 20",maJauge.getMin() == -20);
+	}
+	
+	@Test
+	public void testMaxEgaleMin(){
+		JaugeNaturel maJauge = new JaugeNaturel(-10,-10,-10);
+		assertTrue("val = Min = Max",maJauge.getValeur() == maJauge.getMin() && maJauge.getMin() == maJauge.getMax());
+		
+		maJauge = new JaugeNaturel(-10,-10,-11);
+		assertTrue("val > Min == Max",maJauge.getValeur() > maJauge.getMin() && maJauge.getMin() == maJauge.getMax());
+	}
+	
+	@Test
+	public void testSuperieurIntervalle(){
+		JaugeNaturel maJauge = new JaugeNaturel(-10,-20,-9);
+		assertTrue("Min < Max < val",maJauge.getValeur() > maJauge.getMax() && maJauge.getMin() < maJauge.getMax());
+		
+		maJauge = new JaugeNaturel(-20,-10,-10);
+		assertTrue("Min < Max = val",maJauge.getValeur() == maJauge.getMax() && maJauge.getMin() < maJauge.getMax());
+	}
+}
