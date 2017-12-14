99cf3680fc36e5c5f37e26c9c55546ee13fc8868
GuilhemS31 <guilhem31.susa@hotmail.com>
2017-12-14 15:50:56 +0100

    PASSAGER : classes début

---
+++ Eclipse/src/TransportEnCommun/tec/EtatPassagerTest.java
@@ -0,0 +1,47 @@
+package TransportEnCommun.tec;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class EtatPassagerTest {
+
+
+	@Test
+	public void testEstAssis() {
+		EtatPassager monPassager = new EtatPassager(IEtatPassager.Etat.ASSIS);
+		assertTrue("Est Assis",monPassager.estAssis());
+		assertFalse("N'est pas Exterieur",monPassager.estExterieur());
+		assertFalse("N'est pas Debout",monPassager.estDebout());
+	}
+
+	@Test
+	public void testEstDebout() {
+		EtatPassager monPassager = new EtatPassager(IEtatPassager.Etat.DEBOUT);
+		assertFalse("N'est pas Assis",monPassager.estAssis());
+		assertFalse("N'est pas Exterieur",monPassager.estExterieur());
+		assertTrue("Est Debout",monPassager.estDebout());
+	}
+	
+	@Test
+	public void testEstExterieur() {
+		EtatPassager monPassager = new EtatPassager(IEtatPassager.Etat.DEHORS);
+		assertFalse("N'est pas Assis",monPassager.estAssis());
+		assertTrue("Est Exterieur",monPassager.estExterieur());
+		assertFalse("N'est pas Debout",monPassager.estDebout());
+	}
+
+	@Test
+	public void testEstInterieur() {
+		EtatPassager monPassager = new EtatPassager(IEtatPassager.Etat.ASSIS);
+		assertTrue("Est Interieur",monPassager.estInterieur());
+		
+		monPassager = new EtatPassager(IEtatPassager.Etat.DEBOUT);
+		assertTrue("Est Interieur",monPassager.estInterieur());
+		
+		monPassager = new EtatPassager(IEtatPassager.Etat.DEHORS);
+		assertFalse("N'est pas Interieur",monPassager.estInterieur());
+	}
+
+}
