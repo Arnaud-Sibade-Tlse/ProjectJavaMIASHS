a9f0a4f9d7163f6527016edf6980709de0a12049
DarkVasDehors <sibade.arnaud@gmail.com>
2017-12-18 21:33:24 +0100

    Diagramme +test

--- Eclipse/src/TransportEnCommun/tec/Transport/AutobusTest.java
+++ Eclipse/src/TransportEnCommun/tec/Transport/AutobusTest.java
@@ -29,9 +29,48 @@ public class AutobusTest {
 	public void testAPlaceAssise(){
 		assertTrue("Place Assise",monBus.aPlaceAssise());
 	}
+	
+	@Test
+	public void testPasPlaceAssise() throws UsagerInvalideException{
+		   Usager kaylee = new PassagerStandard("Kaylee", 5);
+		   Usager kaylee2 = new PassagerStandard("Kaylee", 5);
+
+		   kaylee.monterDans(monBus);
+		   kaylee2.monterDans(monBus);
+		assertFalse("Place Assise",monBus.aPlaceAssise());
+	}
 	@Test
 	public void testAPlaceDebout(){
 		assertTrue("Place Debout",monBus.aPlaceDebout());
+	}
+	
+	@Test
+	public void testPasPlaceDebout() throws UsagerInvalideException{
+		   Usager kaylee = new PassagerStandard("Kaylee", 5);
+		   Usager kaylee2 = new PassagerStandard("Kaylee2", 5);
+		   Usager kaylee3 = new PassagerStandard("Kaylee3", 5);
+
+		   kaylee.monterDans(monBus);
+		   kaylee2.monterDans(monBus);
+		   kaylee3.monterDans(monBus);
+
+		assertFalse("Place Debout",monBus.aPlaceDebout());
+	}
+	
+	@Test
+	public void testPasPlaceDeboutDoncDehors() throws UsagerInvalideException{
+		   Usager kaylee = new PassagerStandard("Kaylee", 5);
+		   Usager kaylee2 = new PassagerStandard("Kaylee2", 5);
+		   Usager kaylee3 = new PassagerStandard("Kaylee3", 5);
+		   Usager kaylee4 = new PassagerStandard("Kaylee3", 5);
+
+		   kaylee.monterDans(monBus);
+		   kaylee2.monterDans(monBus);
+		   kaylee3.monterDans(monBus);
+		   kaylee4.monterDans(monBus);
+
+
+		assertFalse("Kaylee4 reste dehors",((PassagerStandard)kaylee4).estDehors()==true);
 	}
 	
 	@Test
@@ -65,5 +104,20 @@ public class AutobusTest {
 		   assertTrue("Sont dessendu", 0 == monBus.getDebout().getValeur() && 0 == monBus.getAssis().getValeur());
 		}
 	
+	@Test
+	public void descentauBonArret() throws UsagerInvalideException{
+			int arretDescente = monBus.getArret()+3;
+		   Usager kaylee = new PassagerStandard("Kaylee", arretDescente);
+		   kaylee.monterDans(monBus);
+		   for(int i = monBus.getArret() ; i<arretDescente+5 ; i++){
+			   monBus.allerArretSuivant();
+			   if(((PassagerStandard)kaylee).getDestination()==i){
+				   break;
+			   }
+		   }
+		   assertTrue("kaylee est descendu au bon moment ", true==((PassagerStandard)kaylee).estDehors());
+	}
+	
+
 	
 }
