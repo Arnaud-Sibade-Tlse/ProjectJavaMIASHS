d21982bab073158fdde405ec0475be5f9d4eb989
GuilhemS31 <guilhem31.susa@hotmail.com>
2017-12-19 14:49:30 +0100

    lvldcnbksjdb onstructeurs

--- Eclipse/src/TransportEnCommun/tec/Passager/PassagerLunatique.java
+++ Eclipse/src/TransportEnCommun/tec/Passager/PassagerLunatique.java
@@ -2,4 +2,9 @@ package TransportEnCommun.tec.Passager;
 
 public class PassagerLunatique extends PassagerAbstract{
 
+	public PassagerLunatique(String nom, int d) {
+		super(nom, d);
+		// TODO Auto-generated constructor stub
+	}
+
 }
--- Eclipse/src/TransportEnCommun/tec/Passager/PassagerStresse.java
+++ Eclipse/src/TransportEnCommun/tec/Passager/PassagerStresse.java
@@ -2,4 +2,9 @@ package TransportEnCommun.tec.Passager;
 
 public class PassagerStresse extends PassagerAbstract{
 
+	public PassagerStresse(String nom, int d) {
+		super(nom, d);
+		// TODO Auto-generated constructor stub
+	}
+
 }
